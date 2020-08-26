c93c8aa466191b243453d2d2f11ddd989c955fc6
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 21:28:57 -0400

    1.00

---
+++ src/variables.java
@@ -0,0 +1,26 @@
+public class variables {
+    public static void main(String[] args) {
+
+  //  {
+  //      int score = 0;
+  //        System.out.println(score);
+  //      score = score + 1;
+  //      System.out.println(score);
+  //  }
+
+        double pay = 62000;
+        System.out.println(pay);
+        pay = pay * 1.1;
+        System.out.println("Year 1: " + pay);
+        pay = pay * 1.1;
+        System.out.println("Year 2: " + pay);
+        pay = pay * 1.1;
+        System.out.println("Year 3: "  + pay);
+        pay = pay * 1.1;
+        System.out.println("Year 4: "  + pay);
+        pay = pay * 1.1;
+        System.out.println("Year 5: "  + pay);
+
+
+    }
+}
